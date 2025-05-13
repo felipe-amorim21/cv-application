51adda5028e9d29111b00bbeeacf97f2de41ed81
Felipe Amorim <felipe21fab@gmail.com>
2025-05-13 13:43:49 -0300

    [FEAT] Add button to dynamically add education entries

--- src/app.jsx
+++ src/app.jsx
@@ -12,11 +12,11 @@ function App(){
         summary: ''
     });
 
-    const [educationInfo, setEducationInfo] = useState({
+    const [educationInfo, setEducationInfo] = useState([{
         schoolName: '',
         degreeName: '',
         endDate: ''
-    });
+    }]);
 
     function handleInfoChange(e, sectionSetter){
         const {name, value} = e.target;
@@ -25,6 +25,19 @@ function App(){
             ...prevInfo,
             [name]: value    
         }));
+    }  
+
+    function handleEducationChange(e, index){
+        const {name, value } = e.target;
+
+        const updatedEducation = [...educationInfo];
+        updatedEducation[index][name] = value;
+        setEducationInfo(updatedEducation)
+    }
+
+
+    const addEducationForm = () => {
+        setEducationInfo(prevEducation => [...prevEducation, {schoolName: '', degreeName: '', endDate: ''}])
     }
 
         
@@ -33,7 +46,14 @@ function App(){
         <div className="cv-application">
             <div className="cv-info">
                 <PersonalInfoForm personalInfo={personalInfo} onPersonalInfoChange={(e) => handleInfoChange(e, setPersonalInfo)}/>
-                <EducationForm educationInfo={educationInfo} onEducationInfoChange={(e) => handleInfoChange(e, setEducationInfo)}/>
+                <div>
+                    {educationInfo.map((educationInfo, index) => (
+                    <EducationForm key={index} educationInfo={educationInfo} onEducationInfoChange={(e) => handleEducationChange(e, index)}/>
+                ))}
+                <button onClick={addEducationForm}>Add</button>
+                </div>
+                
+
             </div>
             <div className="cv-display">
                 <CVDisplay personalInfo={personalInfo} educationInfo={educationInfo}/>
--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -9,9 +9,15 @@ function CVDisplay({ personalInfo, educationInfo }) {
             <p><strong>Phone: </strong> {phoneNumber}</p>
             <p><strong>Summary: </strong></p>
             <p>{summary}</p>
-            <p><strong>School Name: </strong>{schoolName}</p>
-            <p><strong>Degree: </strong>{degreeName}</p>
-            <p><strong>End Date: </strong>{endDate}</p>
+            <h2>Education</h2>
+            {educationInfo.map((education, index) => (
+                <div key={index} className="education-entry">
+                    <p><strong>School Name: </strong>{education.schoolName}</p>
+                    <p><strong>Degree: </strong>{education.degreeName}</p>
+                    <p><strong>End Date: </strong>{education.endDate}</p>
+                </div>
+            ))}
+            
         </div>
 
     );
