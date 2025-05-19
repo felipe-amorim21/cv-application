9d041e3cd5f830b948dd16fca462f60a67d04e13
Felipe Amorim <felipe21fab@gmail.com>
2025-05-19 15:41:53 -0300

    [FEAT] Add css for cv-display

--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -1,32 +1,44 @@
+import './CVDisplay.css'
+
 function CVDisplay({ personalInfo, educationInfo, workExperienceInfo }) {
 
     const { firstName, lastName, email, phoneNumber, summary } = personalInfo
     return (
         <div className="cv-preview">
-            <h1>{firstName} {lastName}</h1>
-            <p><strong>Email: </strong> {email}</p>
-            <p><strong>Phone: </strong> {phoneNumber}</p>
-            <p><strong>Summary: </strong></p>
-            <p>{summary}</p>
-            <h2>Education</h2>
-            {educationInfo.map((education, index) => (
-                <div key={index} className="education-entry">
+            <header className='cv-header'>
+                <h1>{firstName} {lastName}</h1>
+                <div className='cv-contact'>
+                    <p><strong> Email: {email} | Phone: {phoneNumber}</strong></p>
+                </div>
+                <div className='cv-summary'>
+                    <p><strong>Summary: </strong></p>
+                    <p>{summary}</p>
+                </div>
+            </header>
+            <section className='cv-section'>
+                <h2>Education</h2>
+                {educationInfo.map((education, index) => (
+                <div key={index} className="cv-entry">
                     <p><strong>School Name: </strong>{education.schoolName}</p>
                     <p><strong>Degree: </strong>{education.degreeName}</p>
                     <p><strong>End Date: </strong>{education.endDate}</p>
                 </div>
             ))}
-            <h2>Work Experience</h2>
-            {workExperienceInfo.map((work, index) => (
-                <div key={index} className="work-entry">
-                    <p><strong>Job Title: </strong>{work.jobTitle}</p>
-                    <p><strong>Company Name: </strong>{work.companyName}</p>
-                    <p><strong>Start Date: </strong>{work.startDate}</p>
-                    <p><strong>End Date: </strong>{work.endDate}</p>
-                    <p><strong>Description: </strong></p>
+            </section>
+            
+            <section className='cv-section'>
+                <h2>Work Experience</h2>
+                {workExperienceInfo.map((work, index) => (
+                <div key={index} className="cv-entry">
+                    <div className='cv-entry header'>
+                        <p><strong>{work.jobTitle} - {work.companyName}</strong></p>
+                        <p className='cv-date'>{work.startDate} - {work.endDate}</p>
+                    </div>
                     <p>{work.description}</p>
                 </div>
             ))}
+            </section>
+            
             
         </div>
 
